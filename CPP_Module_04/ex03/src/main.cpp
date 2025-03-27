a496f3f6d31b5c944ab392374ba9da667dd1463b
ogokkaya <onurgokkaya@outlook.com>
2025-03-27 10:03:41 +0300

    CPP_Module_04

--- CPP_Module_04/ex03/src/main.cpp
+++ CPP_Module_04/ex03/src/main.cpp
@@ -3,28 +3,22 @@
 
 int main()
 {
-    // 🔹 1. MateriaSource oluştur ve Materia öğren
     MateriaSource* src = new MateriaSource();
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
 
-    // 🔹 2. Karakter oluştur
     Character* player = new Character("Player1");
 
-    // 🔹 3. MateriaSource'tan yeni Materia'lar üret
     AMateria* m1 = src->createMateria("Ice");
     AMateria* m2 = src->createMateria("Cure");
 
-    // 🔹 4. Karakter Materia'ları envanterine ekliyor
     player->equip(m1);
     player->equip(m2);
 
-    // 🔹 5. Başka bir karakter oluştur ve Materia'lar kullanılıyor
     Character* enemy = new Character("Enemy");
     player->use(0, *enemy);  // Ice kullanıldı
     player->use(1, *enemy);  // Cure kullanıldı
 
-    // 🔹 6. Bellek temizlenmeli!
     delete enemy;
     delete player;
     delete src;
